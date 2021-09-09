2b80f456d2c0bc6487602807d098e80092910ff1
demidraksin <greatraksin@icloud.com>
2021-09-09 13:28:41 +0300

    Adding lesson DT theory⚠️

--- 01_lesson_0909/01_data_types_and_input.py
+++ 01_lesson_0909/01_data_types_and_input.py
@@ -1,3 +1,13 @@
+number_a = float(input('Введите число 1: '))
+number_b = float(input('Введите число 2: '))
+'''чтобы сдублировать строчку, нажмите ctrl+d'''
 
+print(f'{number_a} + {number_b} = {number_a + number_b}')
+# форматированная строка - можно вставить значение переменной в строку
+print(f'{number_a} - {number_b} = {number_a - number_b}')
+print(f'{number_a} * {number_b} = {number_a * number_b}')
+print(f'{number_a} // {number_b} = {number_a // number_b}')
+print(f'{number_a} ** {number_b} = {number_a ** number_b}')
 
-
+name = input('Как тебя зовут? ')
+print(f'Привет, {name}!')
